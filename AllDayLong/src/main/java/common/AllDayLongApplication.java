63e8591da2b96a8ef8aa3de623759921948b1618
c4fiber <qudcjf153@gmail.com>
2022-03-08 20:40:10 +0900

    Spring boot Starter
    project Name: All Day Long

---
+++ AllDayLong/src/main/java/common/AllDayLongApplication.java
@@ -0,0 +1,25 @@
+package common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
+@RestController
+public class AllDayLongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllDayLongApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
