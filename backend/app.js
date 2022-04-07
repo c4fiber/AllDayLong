b65cdc24d18e368fe32b7f386ceed8c857be24a7
HyeonseobHwang <flxmfrkdl12@gmail.com>
2022-04-07 21:16:26 +0900

    이미지 업로드 구현

--- backend/app.js
+++ backend/app.js
@@ -3,21 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var dbconfig = require('./config/db');
+var mongoose = require('mongoose');
 
 
-const cors = require("cors");
-const initRoutes = require("./routes/dbconnect");
 
+//dbconnection
+mongoose.connect(dbconfig.url + dbconfig.database);
 
 // Routers
 var userAllRouter = require('./routes/userAll');
 var userInfoRouter = require('./routes/userinfo');
-
-
+var uploadImageRouter = require('./routes/uploadImage');
 var app = express();
 
 
 
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -28,21 +34,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 
+
+
+
+
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('uploadedImages'));
+
+
 
 // routes
 app.use('/users', userAllRouter);
 app.use('/users/:id', userInfoRouter);
-
-
-
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-initRoutes(app);
+app.use('/upload',uploadImageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
