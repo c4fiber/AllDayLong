f6808047182a7a801c2d8276d5fcda260e5979bc
HyeonseobHwang <100767096+HyeonseobHwang@users.noreply.github.com>
2022-05-19 11:45:16 +0900

    image display interval (#33)
    * OCR display 다시 pull request
    
    FE의 pull request로 인하여 이전에 pull request 했던 OCR display 기능 삭제됨 따라서 다시 pull request
    
    * display test with interval
    
    setinterval 사용으로 자동으로 ocr 결과 display

--- frontend/src/components/ImageUpload.vue
+++ frontend/src/components/ImageUpload.vue
@@ -1,56 +1,101 @@
+
 <template>
-  <div>
+<div>
     <h2>이미지 업로드 구현</h2>
     <br>
     <hr>
     <br>
     <form @change="onFileSelected">
-      <input type="file" name="file" id="input-files">
-      <label for="file" >Choose file</label>
-      <input type="submit" value="Submit"  @click="onUpload">
-    </form>
+        <input type="file" name="file" id="input-files">
+        <label for="file" >Choose file</label>
+        <input type="submit" value="Submit"  @click="onUpload">
+    </form> 
+     <div class="ocrtext">
+        <div v-for="(text, index) in ocrtext" :key="index" class="user">
+           
+            <p>{{ text }} </p><br>
+         </div>
 
+   
+    </div>
+    
+    <div class="images">
+    <br> 
+        <div v-for="(imageurl, index2) in images" :key="index2" class="image">
+            <img :src=imageurl alt="imageOCR"/>
+        </div>
+    </div>
+   
+</div>
 
-  </div>
 </template>
 <script>
 
-import axios from 'axios';
-
+    import axios from 'axios';
 export default{
-  name:"ImageUpload",
-  data(){
-    return{
-      selectedFile:null
-    };
-  },
-  methods:{
-    onFileSelected(event){
-      this.selectedFile=event.target.files[0]
-    },
-    onUpload(){
-      var fd = new FormData();
-      fd.append('file',this.selectedFile)
-      axios.post('http://localhost:3000/upload',fd)
-        .then(res =>{
-          console.log(res)
-        });
-    }
-
-  }
+    name:"ImageUpload",
+    data(){
+        return{
+           timerfunc:null,
+            ocrtext: [],
+            images:[],
+        };
+    },   
+    created(){
+        this.timerfunc=setInterval(() =>{ 
+            this.$http.get('/api/upload')
+            .then((res) => {
+            this.ocrtext = res.data.test;
+            this.images=res.data.test2;
+            if(this.ocrtext[0]!=null){
+                clearInterval(this.timerfunc);
+            }
+            })
+            .catch((err) => {
+            console.error(err);
+            });},1000);
+        },
+    methods:{
+        onFileSelected(event){
+            this.selectedFile=event.target.files[0]
+        },
+        onUpload(){
+            var fd = new FormData();
+            fd.append('file',this.selectedFile)
+            axios.post('http://localhost:3000/upload',fd)
+            .then(res =>{
+                console.log(res);
+                })
+            this.timerfunc=setInterval(() =>{ 
+            this.$http.get('/api/upload')
+            .then((res) => {
+            this.ocrtext = res.data.test;
+            this.images=res.data.test2;
+            
+            })
+            .catch((err) => {
+            console.error(err);
+            });},1000);
+            }
+        }
 };
 
-
-
+  
+    
 
 
 
 </script>
 
 <style>
-button{
-  background-color:yellow;
-}
-
+    button{
+        background-color:yellow;
+    }
+    .user {
+        float: left;
+        border: 2px solid black;
+        padding: 15px;
+        margin: 20px;
+    }
 </style>
 
