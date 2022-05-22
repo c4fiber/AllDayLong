783b8f8b97d74c4148e8bc3fb4d2fe9874962797
GnoonJo <68181274+gnoon9697@users.noreply.github.com>
2022-05-23 01:04:33 +0900

    Update ImageUpload.vue
    Current File이 더 최신버전

--- frontend/src/components/ImageUpload.vue
+++ frontend/src/components/ImageUpload.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div>
     <h2>이미지 업로드 구현</h2>
@@ -5,52 +6,87 @@
     <hr>
     <br>
     <form @change="onFileSelected">
-      <input type="file" name="file" id="input-files">
-      <label for="file" >Choose file</label>
-      <input type="submit" value="Submit"  @click="onUpload">
-    </form>
-
-
+        <input type="file" name="file" id="input-files">
+        <label for="file" >Choose file</label>
+        <input type="submit" value="Submit"  @click="onUpload">
+    </form> 
+     <div class="ocrtext">
+        <div v-for="(text, index) in ocrtext" :key="index" class="user">
+           
+            <p>{{ text }} </p><br>
+         </div>
+    </div>
+    <br>
+    <div class="images">
+      <div v-for="(imageurl, index2) in images" :key="index2" class="image">
+        <img :src=imageurl alt="imageOCR"/>
+      </div>
+    </div>
   </div>
 </template>
-<script>
 
+<script>
 import axios from 'axios';
 
 export default{
   name:"ImageUpload",
   data(){
     return{
-      selectedFile:null
+        timerfunc:null,
+        ocrtext: [],
+        images:[],
     };
-  },
+  },   
+  created(){
+    this.timerfunc=setInterval(() =>{ 
+        this.$http.get('/api/upload')
+        .then((res) => {
+        this.ocrtext = res.data.test;
+        this.images=res.data.test2;
+        if(this.ocrtext[0]!=null){
+            clearInterval(this.timerfunc);
+        }
+        })
+        .catch((err) => {
+        console.error(err);
+        });},1000);
+    },
   methods:{
     onFileSelected(event){
-      this.selectedFile=event.target.files[0]
+        this.selectedFile=event.target.files[0]
     },
     onUpload(){
-      var fd = new FormData();
-      fd.append('file',this.selectedFile)
-      axios.post('http://localhost:3000/upload',fd)
+        var fd = new FormData();
+        fd.append('file',this.selectedFile)
+        axios.post('http://localhost:3000/upload',fd)
         .then(res =>{
-          console.log(res)
-        });
+            console.log(res);
+            })
+        this.timerfunc=setInterval(() =>{ 
+        this.$http.get('/api/upload')
+        .then((res) => {
+        this.ocrtext = res.data.test;
+        this.images=res.data.test2;
+        
+        })
+        .catch((err) => {
+        console.error(err);
+        });},1000);
+        }
     }
-
-  }
 };
-
-
-
-
-
 
 </script>
 
 <style>
-button{
-  background-color:yellow;
-}
-
+    button{
+        background-color:yellow;
+    }
+    .user {
+        float: left;
+        border: 2px solid black;
+        padding: 15px;
+        margin: 20px;
+    }
 </style>
 
