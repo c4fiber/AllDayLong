45048e4c8e083f38293b09d66c011ae107bcbd0c
Jokyungseo <kaya1108@naver.com>
2022-05-27 01:23:43 +0900

    AboutView 및 HomeView 수정 통합과정 (#48)
    * 1차
    
    일단 테스트
    
    * Update README.md
    
    * Revert "Update README.md"
    
    This reverts commit 39bdaad6dadcc8b8101a7aad05d5b80627939516.
    
    * 0512
    
    0512
    
    * 0519
    
    mobile responsive image select and some fixes
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Update HomeView.vue
    
    * Update README.md
    
    기존 md 형태로 수정
    
    * Update README.md
    
    오타 수정 (콘솔에에 -> 콘솔에)
    
    * Delete demo.png.json
    
    demo 파일이 너무 많아서 일괄 삭제처리
    
    * Update uploadImage.js
    
    Current File이 Incoming File 보다 더 최신버전
    
    * Update index.html
    
    Current File이 더 최신 버전
    
    * Update App.vue
    
    Current File이 더 최신버전
    
    * Update DisplayOCR.vue
    
    Current File이 더 최신버전
    
    * Update UserAll.vue
    
    Current File이 더 최신버전
    
    * Update index.js
    
    Current File이 더 최신 버전
    
    * Update ImageUpload.vue
    
    Current File이 더 최신버전
    
    * Update main.js
    
    코드 Refine
    
    * Update HomeView.vue
    
    모바일 통합 시도
    
    * Update HomeView.vue
    
    drag drop mq 적용
    
    * 0526
    
    homeview 수정 및 디자인 통일
    aboutview 소개글
    
    * Update users.json
    
    Co-authored-by: GnoonJo <68181274+gnoon9697@users.noreply.github.com>
    Co-authored-by: GnoonJo <68181274+GnoonJo@users.noreply.github.com>

--- frontend/src/components/AboutView.vue
+++ frontend/src/components/AboutView.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="about">
-    <h1>ORCA Introduction</h1>
+    <h2>OCR Introduction</h2>
     <p>
-      ORCA는 한국어 인식 OCR 소프트웨어입니다.
+      OCR(Optical Character Recognition)은 이미지 속의 텍스트를 실제 문자 텍스트 형태로 변환시키는 기술이다. Text Detection 단계와 Text Recognition 단계로
+      구성되어 있다.
+      대표적인 OCR 엔진으로는 EasyOCR, Tesseract OCR이 있다.
     </p>
+    <h2>Then what is ORCA?</h2>
+    <p>
+      ORCA는 기존의 OCR 모델이 영어와 숫자에 중점을 두고, 한글의 경우 인쇄글자와 문서에만 서비스를 제공하며 그마저도 인식 정확도가 낮은 문제점들을 보완, 개선한 새로운 한국어 OCR 모델이다.
+    </p>
+    <h2>Applied OCR model</h2>
+      <p>
+        ORCA은 기존 OCR 데이터의 특징을 추출하여 패턴을 파악하는 CNN(Convolution Neural Network)과 이전 단계의 학습 데이터를 현재 학습에 반영시키는 RNN(Recurrent Neural Network)을 합친 
+      </p>
   </div>
 </template>
