7818ea49d91b6a6f344a2abd56cfb067ebfdc12c
FL <refrain2k@gmail.com>
2022-06-02 02:46:44 +0900

    3분할 OCR 구현

--- backend/ocr-modules/OCR-Attn/RunOCREngineV3.py
+++ backend/ocr-modules/OCR-Attn/RunOCREngineV3.py
@@ -9,12 +9,14 @@ import warnings
 warnings.filterwarnings("ignore", category=UserWarning, module="torch.nn.functional")
 
 
-def OCREngine(image):
+def OCREngineV3(image):
+    path_abs = os.path.dirname(os.path.abspath(__file__))
+
     # 이하 opt을 변경하여 기학습된 모델을 교체할 수 있음
     #  Parameters가 변경되었을 경우 OCR-Attn/text_recognize/recognition.py을 참조할 것.
 
-    # 1차 모델 TPS-VGG-BiLSTM-Attn
-    path_abs = os.path.dirname(os.path.abspath(__file__))
+    # Model #1
+    # 3split_ocr (TPS-VGG-BiLSTM-Attn)
     opt = SimpleNamespace()
     opt.detect_trained_model = f"{path_abs}/models/craft_mlt_25k.pth"
     opt.detect_result_folder = f"{path_abs}/images/box/"
@@ -24,8 +26,9 @@ def OCREngine(image):
     opt.recognize_FeatureExtraction = "VGG"
     opt.recognize_SequenceModeling = "BiLSTM"
     opt.recognize_Prediction = "Attn"
-    # 2차 모델 TPS-VGG-BiLSTM-Attn
-    path_abs = os.path.dirname(os.path.abspath(__file__))
+
+    # Model #2
+    # 3split_htr (TPS-VGG-BiLSTM-Attn)
     opt2 = SimpleNamespace()
     opt2.detect_trained_model = f"{path_abs}/models/craft_mlt_25k.pth"
     opt2.detect_result_folder = f"{path_abs}/images/box/"
@@ -35,8 +38,9 @@ def OCREngine(image):
     opt2.recognize_FeatureExtraction = "VGG"
     opt2.recognize_SequenceModeling = "BiLSTM"
     opt2.recognize_Prediction = "Attn"
-    # 3차 모델 None-VGG-BiLSTM-CTC
-    path_abs = os.path.dirname(os.path.abspath(__file__))
+
+    # Model #3
+    # 3split_korean_g2 (None-VGG-BiLSTM-CTC)
     opt3 = SimpleNamespace()
     opt3.detect_trained_model = f"{path_abs}/models/craft_mlt_25k.pth"
     opt3.detect_result_folder = f"{path_abs}/images/box/"
@@ -50,32 +54,32 @@ def OCREngine(image):
     # OCR
     start = OCR(opt)
     result1 = start.run(image)
-    print("#Model 1:", opt.detect_trained_model,
+    print("#Model 1:", opt.recognize_saved_model,
           "\n Network Model :", opt.recognize_Transformation, opt.recognize_FeatureExtraction,
           opt.recognize_SequenceModeling, opt.recognize_Prediction,
           "\n Results :\n", result1)
 
     start = OCR(opt2)
     result2 = start.run(image)
-    print("#Model 2:", opt2.detect_trained_model,
+    print("#Model 2:", opt2.recognize_saved_model,
           "\n Network Model :", opt2.recognize_Transformation, opt2.recognize_FeatureExtraction,
           opt2.recognize_SequenceModeling, opt2.recognize_Prediction,
           "\n Results :\n", result2)
 
     start = OCR(opt3)
     result3 = start.run(image)
-    print("#Model 3:", opt3.detect_trained_model,
+    print("#Model 3:", opt3.recognize_saved_model,
           "\n Network Model :", opt3.recognize_Transformation, opt3.recognize_FeatureExtraction,
           opt3.recognize_SequenceModeling, opt3.recognize_Prediction,
           "\n Results :\n", result3)
 
-    # OCR 결과 변환
+    # 결과 비교 및 저장
     json_path = os.path.dirname(image) + "/" + os.path.basename(image) + ".json"
     LIST2JSON.tojsontrio(result1, result2, result3, json_path)
     DrawTextBox.Draw(image, result1)
 
 
 if __name__ == '__main__':
-    image_path = sys.argv[1]  # 명령행 인자
-    OCREngine(image_path)
-    # OCREngine('C:/Users/Fair/PycharmProjects/Module/OCR-Attn/test/demo.png')
+    image_path = sys.argv[1]  # args
+    OCREngineV3(image_path)
+    # OCREngineV3('C:/Users/Fair/PycharmProjects/Module/OCR-Attn/test/demo.png')
