0c16f19569ab280688f70aba8b57b4d720b4794f
Jokyungseo <kaya1108@naver.com>
2022-06-06 18:09:50 +0900

    About 컴포넌트 제작 (#59)
    * 1차
    
    일단 테스트
    
    * Update README.md
    
    * Revert "Update README.md"
    
    This reverts commit 39bdaad6dadcc8b8101a7aad05d5b80627939516.
    
    * 0512
    
    0512
    
    * 0519
    
    mobile responsive image select and some fixes
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Update HomeView.vue
    
    * Update README.md
    
    기존 md 형태로 수정
    
    * Update README.md
    
    오타 수정 (콘솔에에 -> 콘솔에)
    
    * Delete demo.png.json
    
    demo 파일이 너무 많아서 일괄 삭제처리
    
    * Update uploadImage.js
    
    Current File이 Incoming File 보다 더 최신버전
    
    * Update index.html
    
    Current File이 더 최신 버전
    
    * Update App.vue
    
    Current File이 더 최신버전
    
    * Update DisplayOCR.vue
    
    Current File이 더 최신버전
    
    * Update UserAll.vue
    
    Current File이 더 최신버전
    
    * Update index.js
    
    Current File이 더 최신 버전
    
    * Update ImageUpload.vue
    
    Current File이 더 최신버전
    
    * Update main.js
    
    코드 Refine
    
    * Update HomeView.vue
    
    모바일 통합 시도
    
    * 3rd (#42)
    
    * 1차
    
    일단 테스트
    
    * Update README.md
    
    * Revert "Update README.md"
    
    This reverts commit 39bdaad6dadcc8b8101a7aad05d5b80627939516.
    
    * 0512
    
    0512
    
    * 0519
    
    mobile responsive image select and some fixes
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Update HomeView.vue
    
    * Update README.md
    
    기존 md 형태로 수정
    
    * Update README.md
    
    오타 수정 (콘솔에에 -> 콘솔에)
    
    * Delete demo.png.json
    
    demo 파일이 너무 많아서 일괄 삭제처리
    
    * Update uploadImage.js
    
    Current File이 Incoming File 보다 더 최신버전
    
    * Update index.html
    
    Current File이 더 최신 버전
    
    * Update App.vue
    
    Current File이 더 최신버전
    
    * Update DisplayOCR.vue
    
    Current File이 더 최신버전
    
    * Update UserAll.vue
    
    Current File이 더 최신버전
    
    * Update index.js
    
    Current File이 더 최신 버전
    
    * Update ImageUpload.vue
    
    Current File이 더 최신버전
    
    * Update main.js
    
    코드 Refine
    
    * Update HomeView.vue
    
    모바일 통합 시도
    
    Co-authored-by: GnoonJo <68181274+gnoon9697@users.noreply.github.com>
    
    * Update HomeView.vue
    
    drag drop mq 적용
    
    * 0526
    
    homeview 수정 및 디자인 통일
    aboutview 소개글
    
    * Update users.json
    
    * 0531
    
    Almost complete aboutView
    
    * Delete users.json
    
    * Update HomeView.vue
    
    Co-authored-by: GnoonJo <68181274+gnoon9697@users.noreply.github.com>
    Co-authored-by: GnoonJo <68181274+GnoonJo@users.noreply.github.com>

--- frontend/src/components/AboutView.vue
+++ frontend/src/components/AboutView.vue
@@ -6,13 +6,44 @@
       구성되어 있다.
       대표적인 OCR 엔진으로는 EasyOCR, Tesseract OCR이 있다.
     </p>
-    <h2>Then what is ORCA?</h2>
+    <h2>Related OCR Technology Status</h2>
+      <h4><span style="color:#009dff;">Tesseract OCR</span></h4>
     <p>
-      ORCA는 기존의 OCR 모델이 영어와 숫자에 중점을 두고, 한글의 경우 인쇄글자와 문서에만 서비스를 제공하며 그마저도 인식 정확도가 낮은 문제점들을 보완, 개선한 새로운 한국어 OCR 모델이다.
+      특징: HP 연구소에서 개발한 오픈소스 OCR, 높은 정확도
+      문제점: 한글 인식률이 저조, 자체적인 인식률 개선에 한계가 존재
     </p>
+      <h4><span style="color:#2db245;">NAVER CLOVA</span></h4>
+    <p>
+      특징: 배열된 문자와 읽는 순서를 이용하여 AI 모델을 바탕으로 인식
+      문제점: 지속적인 비용 지불, 개발자를 대상으로 하는 서비스로 접근성이 낮음
+    </p>
+    <h2>Development Background</h2>
+    <p>
+      영어 위주의 OCR 기술은 한글의 인식률이 저조하고, 개발자 위주의 서비스를 하여 접근성이 떨어지는 단점들이 존재한다.
+    </p>
+    <h2>Then what is <span style="color:#3cb1f9;">ORCA</span></h2>
+    <p>
+      <span style="color:#3cb1f9;">ORCA</span>는 위에 나열한 문제점들을 보완하여 누구나 쉽게 체험 가능한 한국어 OCR 모델이다.
+    </p>
+    <!-- <p>
+      <span style="color:#3cb1f9;">ORCA</span>는 기존의 OCR 모델이 영어와 숫자에 중점을 두고, 한글의 경우 인쇄글자와 문서에만 서비스를 제공하며 그마저도 인식 정확도가 낮은 문제점들을 보완, 개선한 새로운 한국어 OCR 모델이다.
+    </p> -->
     <h2>Applied OCR model</h2>
-      <p>
-        ORCA은 기존 OCR 데이터의 특징을 추출하여 패턴을 파악하는 CNN(Convolution Neural Network)과 이전 단계의 학습 데이터를 현재 학습에 반영시키는 RNN(Recurrent Neural Network)을 합친 
-      </p>
+    <p>
+      ORCA은 기존 OCR 데이터의 특징을 추출하여 패턴을 파악하는 CNN(Convolution Neural Network)과 이전 단계의 학습 데이터를 현재 학습에 반영시키는 RNN(Recurrent
+      Neural Network)을 합친
+    </p>
+    <h2>사용 TOOLS</h2> 
+    <img id = "Tool_img" src="@/assets/image/Applied_Tool.png" alt="">
   </div>
 </template>
+<style>
+  p{
+    color: rgba(0, 0, 0, 0.8);
+  }
+  #Tool_img{
+    width: 60%;
+    height: 300px;
+    margin: 0 20%;
+}
+</style>
